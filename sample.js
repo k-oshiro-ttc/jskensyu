39482750412b62eb09b14765853b3d2b61f80301
k-oshiro-ttc <63641558+k-oshiro-ttc@users.noreply.github.com>
2021-03-29 18:52:08 +0900

    描画処理のエラー有

--- sample.js
+++ sample.js
@@ -1,29 +1,3 @@
-class Breakout {
-    #canvas;
-    #ball;
-    #paddle;
-    #brick;
-    constructor(canvas, score, lives, ballRadius, dx, dy, paddleHeight, paddleWidth, paddleX) {
-        this.canvas = new Canvas(canvas, score, lives);
-        this.ball = new Ball(ballRadius, dx, dy);
-        this.paddle = new Paddle(paddleHeight, paddleWidth, paddleX);
-        this.brick = new Brick(brickRowCount, brickColumnCount, brickWidth, brickHeight, brickPadding, brickOffsetTop, brickOffsetLeft);
-    }
-
-    get getCanvas() {
-        return this.#canvas;
-    }
-    get getBall() {
-        return this.#ball;
-    }
-    get getPaddle() {
-        return this.#paddle;
-    }
-    get getBrick() {
-        return this.#brick;
-    }
-}
-
 // canvasの設定
 class Canvas {
     #canvas;
@@ -40,6 +14,37 @@ class Canvas {
         this.#score = score;
         this.#lives = lives;
     }
+
+    getCanvas() {
+        return this.#canvas;
+    }
+    getCtx() {
+        return this.#ctx;
+    }
+    getX() {
+        return this.#x;
+    }
+    setX(x) {
+        this.#x = x;
+    }
+    getY() {
+        return this.#y;
+    }
+    setY(y) {
+        this.#y = y;
+    }
+    getScore() {
+        return this.#score;
+    }
+    setScore(score) {
+        this.#score = score;
+    }
+    getLives() {
+        return this.#lives;
+    }
+    setLives(lives) {
+        this.#lives = lives;
+    }
 }
 
 // ballの設定
@@ -50,6 +55,22 @@ class Ball {
     constructor(ballRadius, dx, dy) {
         this.#ballRadius = ballRadius;
         this.#dx = dx;
+        this.#dy = dy;
+    }
+
+    getBallRadius() {
+        return this.#ballRadius;
+    }
+    getDx() {
+        return this.#dx;
+    }
+    setDx(dx) {
+        this.#dx = dx;
+    }
+    getDy() {
+        return this.#dy;
+    }
+    setDy(dy) {
         this.#dy = dy;
     }
 }
@@ -69,10 +90,28 @@ class Paddle {
         this.#leftPressed = false;
     }
 
-    set setRightPressed(rightPressed) {
+    getPaddleHeight() {
+        return this.#paddleHeight;
+    }
+    getPaddleWidth() {
+        return this.#paddleWidth;
+    }
+    getPaddleX() {
+        return this.#paddleX;
+    }
+    setPaddleX(paddleX) {
+        this.#paddleX = paddleX;
+    }
+    getRightPressed() {
+        return this.#rightPressed;
+    }
+    setRightPressed(rightPressed) {
         this.#rightPressed = rightPressed;
     }
-    set setLeftPressed(leftPressed) {
+    getLeftPressed() {
+        return this.#leftPressed;
+    }
+    setLeftPressed(leftPressed) {
         this.#leftPressed = leftPressed;
     }
 }
@@ -98,24 +137,42 @@ class Brick {
         this.#bricks = [];
     }
 
-    get getBrickRowCount() {
+    getBrickRowCount() {
         return this.#brickRowCount;
     }
-    get getBrickColumnCount() {
+    getBrickColumnCount() {
         return this.#brickColumnCount;
     }
-    setBricks(column) {
+    getBrickWidth() {
+        return this.#brickWidth;
+    }
+    getBrickHeight() {
+        return this.#brickHeight;
+    }
+    getBrickPadding() {
+        return this.#brickPadding;
+    }
+    getBrickOffsetTop() {
+        return this.#brickOffsetTop;
+    }
+    getBrickOffsetLeft() {
+        return this.#brickOffsetLeft;
+    }
+    getBricks(column, row) {
+        return this.#bricks[column][row];
+    }
+    setBricksClear(column) {
         this.#bricks[column] = [];
     }
     setBricks(column, row, x, y, status) {
-        this.#bricks[column][row] = { x: x, y: y, status: status};
+        this.#bricks[column][row] = { x: x, y: y, status: status };
     }
 }
 
 // brickの数を設定
 function setBricks(brick) {
     for(var c = 0; c < brick.getBrickColumnCount(); c++) {
-        brick.setBricks(c);
+        brick.setBricksClear(c);
         for(var r = 0; r < brick.getBrickRowCount(); r++) {
             brick.setBricks(c, r, 0, 0, 1);
         }
@@ -123,150 +180,150 @@ function setBricks(brick) {
 }
 
 // EventListenerの追加
-function addEventListeners(breakout) {
-    document.addEventListener("keydown", keyDownHandler, breakout.getPaddle(), false);
-    document.addEventListener("keyup", keyUpHandler, breakout.getPaddle(), false);
-    document.addEventListener("mousemove", mouseMoveHandler, false);
+function addEventListeners(canvas, paddle) {
+    document.addEventListener("keydown", keyDownHandler, paddle, false);
+    document.addEventListener("keyup", keyUpHandler, paddle, false);
+    document.addEventListener("mousemove", mouseMoveHandler, canvas, paddle, false);
 }
 
 // キーが押された時の処理
 function keyDownHandler(paddle, e) {
     if(e.key == "Right" || e.key == "ArrowRight") {
-        paddle.setRightPressed = true;
+        paddle.setRightPressed(true);
     }
     else if(e.key == "Left" || e.key == "ArrowLeft") {
-        paddle.setLeftPressed = true;
+        paddle.setLeftPressed(true);
     }
 }
 
 // キーが離された時の処理
 function keyUpHandler(paddle, e) {
     if(e.key == "Right" || e.key == "ArrowRight") {
-        paddle.setRightPressed = false;
+        paddle.setRightPressed(false);
     }
     else if(e.key == "Left" || e.key == "ArrowLeft") {
-        paddle.setLeftPressed = false;
+        paddle.setLeftPressed(false);
     }
 }
 
 // マウスを動かしたときの処理
-function mouseMoveHandler(e) {
-    var relativeX = e.clientX - canvas.canvas.offsetLeft;
-    if(relativeX > 0 && relativeX < canvas.canvas.width) {
-        paddle.paddleX = relativeX - paddle.paddleWidth/2;
+function mouseMoveHandler(canvas, paddle, e) {
+    var relativeX = e.clientX - canvas.getCanvas().offsetLeft;
+    if(relativeX > 0 && relativeX < canvas.getCanvas().width) {
+        paddle.setPaddleX() = relativeX - paddle.getPaddleWidth() / 2;
     }
 }
 
 // 描画処理
-function draw() {
-    canvas.ctx.clearRect(0, 0, canvas.canvas.width, canvas.canvas.height);
-    drawBricks();
-    drawBall();
-    drawPaddle();
-    drawScore();
-    drawLives();
-    collisionDetection();
+function draw(canvas, ball, paddle, brick) {
+    canvas.getCtx().clearRect(0, 0, canvas.getCanvas().width, canvas.getCanvas().height);
+    drawBricks(canvas, brick);
+    drawBall(canvas, ball);
+    drawPaddle(canvas, paddle);
+    drawScore(canvas);
+    drawLives(canvas);
+    collisionDetection(canvas, ball, brick);
 
-    if(canvas.x + ball.dx > canvas.canvas.width - ball.ballRadius || canvas.x + ball.dx < ball.ballRadius) {
-        ball.dx = -ball.dx;
+    if(canvas.getX() + ball.getDx() > canvas.getCanvas().width - ball.getBallRadius() || canvas.getX() + ball.getDx() < ball.getBallRadius()) {
+        ball.setDx(-ball.getDx());
     }
-    if(canvas.y + ball.dy < ball.ballRadius) {
-        ball.dy = -ball.dy;
+    if(canvas.getY() + ball.getDy() < ball.getBallRadius()) {
+        ball.setDy(-ball.getDy());
     }
-    else if(canvas.y + ball.dy > canvas.canvas.height - ball.ballRadius) {
-        if(canvas.x > paddle.paddleX && canvas.x < paddle.paddleX + paddle.paddleWidth) {
-            ball.dy = -ball.dy;
+    else if(canvas.getY() + ball.getDy() > canvas.getCanvas().height - ball.getBallRadius()) {
+        if(canvas.getX() > paddle.getPaddleX() && canvas.getX() < paddle.getPaddleX() + paddle.getPaddleWidth()) {
+            ball.setDy(-ball.getDy());
         }
         else {
-            canvas.lives--;
-            if(!canvas.lives) {
+            canvas.setLives(canvas.getLives() - 1);
+            if(!canvas.getLives()) {
                 alert("GAME OVER");
                 document.location.reload();
             }
             else {
-                canvas.x = canvas.canvas.width / 2;
-                canvas.y = canvas.canvas.height - 30;
-                ball.dx = 3;
-                ball.dy = -3;
-                paddle.paddleX = (canvas.canvas.width - paddle.paddleWidth) / 2;
+                canvas.setX(canvas.getCanvas().width / 2);
+                canvas.setY(canvas.getCanvas().height - 30);
+                ball.setDx(3);
+                ball.setDy(-3);
+                paddle.setPaddleX((canvas.getCanvas().width - paddle.getPaddleWidth()) / 2);
             }
         }
     }
 
-    if(paddle.rightPressed && paddle.paddleX < canvas.canvas.width - paddle.paddleWidth) {
-        paddle.paddleX += 7;
+    if(paddle.getRightPressed() && paddle.getPaddleX() < canvas.getCanvas().width - paddle.getPaddleWidth()) {
+        paddle.setPaddleX(paddle.getPaddleX() + 7);
     }
-    else if(paddle.leftPressed && paddle.paddleX > 0) {
-        paddle.paddleX -= 7;
+    else if(paddle.getLeftPressed() && paddle.getPaddleX() > 0) {
+        paddle.setPaddleX(paddle.getPaddleX() - 7);
     }
 
-    canvas.x += ball.dx;
-    canvas.y += ball.dy;
+    canvas.setX(canvas.getX() + ball.getDx());
+    canvas.setY(canvas.getY() + ball.getDy());
     requestAnimationFrame(draw);
 }
 
 // brickの描画処理
-function drawBricks() {
-    for(var c = 0; c < brick.brickColumnCount; c++) {
-        for(var r = 0; r < brick.brickRowCount; r++) {
-            if(brick.bricks[c][r].status == 1) {
-                var brickX = (r * (brick.brickWidth + brick.brickPadding)) + brick.brickOffsetLeft;
-                var brickY = (c * (brick.brickHeight + brick.brickPadding)) + brick.brickOffsetTop;
-                brick.bricks[c][r].x = brickX;
-                brick.bricks[c][r].y = brickY;
-                canvas.ctx.beginPath();
-                canvas.ctx.rect(brickX, brickY, brick.brickWidth, brick.brickHeight);
-                canvas.ctx.fillStyle = "#0095DD";
-                canvas.ctx.fill();
-                canvas.ctx.closePath();
+function drawBricks(canvas, brick) {
+    for(var c = 0; c < brick.getBrickColumnCount(); c++) {
+        for(var r = 0; r < brick.getBrickRowCount(); r++) {
+            if(brick.getBricks(c, r).status == 1) {
+                var brickX = (r * (brick.getBrickWidth() + brick.getBrickPadding())) + brick.getBrickOffsetLeft();
+                var brickY = (c * (brick.getBrickHeight() + brick.getBrickPadding())) + brick.getBrickOffsetTop();
+                brick.getBricks(c, r).x = brickX;
+                brick.getBricks(c, r).y = brickY;
+                canvas.getCtx().beginPath();
+                canvas.getCtx().rect(brickX, brickY, brick.getBrickWidth(), brick.getBrickHeight());
+                canvas.getCtx().fillStyle = "#0095DD";
+                canvas.getCtx().fill();
+                canvas.getCtx().closePath();
             }
         }
     }
 }
 
 // ballの描画処理
-function drawBall() {
-    canvas.ctx.beginPath();
-    canvas.ctx.arc(canvas.x, canvas.y, ball.ballRadius, 0, Math.PI*2);
-    canvas.ctx.fillStyle = "#0095DD";
-    canvas.ctx.fill();
-    canvas.ctx.closePath();
+function drawBall(canvas, ball) {
+    canvas.getCtx().beginPath();
+    canvas.getCtx().arc(canvas.getX(), canvas.getY(), ball.getBallRadius(), 0, Math.PI*2);
+    canvas.getCtx().fillStyle = "#0095DD";
+    canvas.getCtx().fill();
+    canvas.getCtx().closePath();
 }
 
 // paddleの描画処理
-function drawPaddle() {
-    canvas.ctx.beginPath();
-    canvas.ctx.rect(paddle.paddleX, canvas.canvas.height - paddle.paddleHeight, paddle.paddleWidth, paddle.paddleHeight);
-    canvas.ctx.fillStyle = "#0095DD";
-    canvas.ctx.fill();
-    canvas.ctx.closePath();
+function drawPaddle(canvas, paddle) {
+    canvas.getCtx().beginPath();
+    canvas.getCtx().rect(paddle.getPaddleX(), canvas.getCanvas().height - paddle.getPaddleHeight(), paddle.getPaddleWidth(), paddle.getPaddleHeight());
+    canvas.getCtx().fillStyle = "#0095DD";
+    canvas.getCtx().fill();
+    canvas.getCtx().closePath();
 }
 
 // scoreの描画処理
-function drawScore() {
-    canvas.ctx.font = "16px Arial";
-    canvas.ctx.fillStyle = "#0095DD";
-    canvas.ctx.fillText("Score: " + canvas.score, 8, 20);
+function drawScore(canvas) {
+    canvas.getCtx().font = "16px Arial";
+    canvas.getCtx().fillStyle = "#0095DD";
+    canvas.getCtx().fillText("Score: " + canvas.getScore(), 8, 20);
 }
 
 // livesの描画処理
-function drawLives() {
-    canvas.ctx.font = "16px Arial";
-    canvas.ctx.fillStyle = "#0095DD";
-    canvas.ctx.fillText("Lives: " + canvas.lives, canvas.canvas.width - 65, 20);
+function drawLives(canvas) {
+    canvas.getCtx().font = "16px Arial";
+    canvas.getCtx().fillStyle = "#0095DD";
+    canvas.getCtx().fillText("Lives: " + canvas.getLives(), canvas.getCanvas().width - 65, 20);
 }
 
 // brickとballが衝突したときの処理
-function collisionDetection() {
-    for(var c = 0; c < brick.brickColumnCount; c++) {
-        for(var r = 0; r < brick.brickRowCount; r++) {
-            var b = brick.bricks[c][r];
+function collisionDetection(canvas, ball, brick) {
+    for(var c = 0; c < brick.getBrickColumnCount(); c++) {
+        for(var r = 0; r < brick.getBrickRowCount(); r++) {
+            var b = brick.getBricks(c, r);
             if(b.status == 1) {
-                if(canvas.x > b.x && canvas.x < b.x + brick.brickWidth && canvas.y > b.y && canvas.y < b.y + brick.brickHeight) {
-                    ball.dy = -ball.dy;
+                if(canvas.getX() > b.x && canvas.getX() < b.x + brick.getBrickWidth() && canvas.getY() > b.y && canvas.getY() < b.y + brick.getBrickHeight()) {
+                    ball.setDy(-ball.getDy());
                     b.status = 0;
-                    canvas.score++;
-                    if(canvas.score == brick.brickRowCount * brick.brickColumnCount) {
+                    canvas.setScore(canvas.getScore() + 1);
+                    if(canvas.getScore() == brick.getBrickRowCount() * brick.getBrickColumnCount()) {
                         alert("YOU WIN, CONGRATS!");
                         document.location.reload();
                     }
